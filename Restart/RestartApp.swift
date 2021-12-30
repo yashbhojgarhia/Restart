60234b002996b2f8dba5e2ac8549d88a35caabe1
Yash Bhojgarhia <yashbhojgarhia@gmail.com>
2021-12-30 15:58:04 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Yash Bhojgarhia on 30/12/21.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
